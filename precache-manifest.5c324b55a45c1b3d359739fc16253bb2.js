3fdce72157340dc94e0dea63042bb6e3723cb1ec
KseniiaMazan <ksusamazan@gmail.com>
2019-09-10 13:09:30 +0300

    Updates

---
+++ precache-manifest.5c324b55a45c1b3d359739fc16253bb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e95399ab8ed71b3bafd4636375482c",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "e92503863ca58bd8ce29",
+    "url": "/react_static-list-of-posts/static/css/main.a1d6516a.chunk.css"
+  },
+  {
+    "revision": "a30e34cb83e4cc2fb81f",
+    "url": "/react_static-list-of-posts/static/js/2.91cf3685.chunk.js"
+  },
+  {
+    "revision": "e92503863ca58bd8ce29",
+    "url": "/react_static-list-of-posts/static/js/main.f0777cff.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
